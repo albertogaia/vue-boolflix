dff87fe963fd86263d6b8d1f90ff49a380b12b92
albertogaia <alberto.gaia95@gmail.com>
2021-09-24 17:30:41 +0200

    Messo bandierine belle

--- src/components/VideoCard.vue
+++ src/components/VideoCard.vue
@@ -2,13 +2,18 @@
   <div>
         <div class="item-info">Titolo<h3 class="item-title">{{item.title}}</h3></div>
         <div class="item-info">Titolo originale<h3 class="item-originalTitle">{{item.original_title}}</h3></div>
-        <div class="item-info">Lingua<h3 class="item-language">{{item.original_language}}</h3></div>
+        <div class="item-info">Lingua<h3 class="item-language"><country-flag :country='myLang(item.original_language)'/></h3></div>
         <div class="item-info">Voto<h3 class="item-vote">{{item.vote_average}}</h3></div>
   </div>
 </template>
 
 <script>
+import CountryFlag from 'vue-country-flag'
+
 export default {
+    components: {
+        CountryFlag,
+    },
     name: 'VideoCard',
 
     props: ['item', 'arrayList'],
@@ -16,12 +21,14 @@ export default {
         return {
         }
     },
-
-    methods:{
+    methods: {
+        myLang(lang){
+            if(lang == 'en') return 'gb' 
+            return lang
+        },
     },
 
-    created() {
-    },
+
 }
 </script>
 
